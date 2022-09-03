f20fd91db2f79ec9bd8071a902ea5b148477e879
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-03 12:46:49 -0500

    ADD: Ejercicio 2 y Angular Routing

--- ejercicio01/src/app/app.module.ts
+++ ejercicio01/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ejercicio2Component } from './ejercicio2/ejercicio2.component';
+import { Ejercicio1Component } from './ejercicio1/ejercicio1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Ejercicio1Component,
+    Ejercicio2Component
   ],
   imports: [
     BrowserModule,
